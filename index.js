0593a4881a5057ba2e6ee983503a3fb67ec97970
CadCreations <davescottmay@gmail.com>
2022-01-25 12:47:42 -0500

    first pass

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+
+
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+   let weeklyAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+   weeklyExpenses = weeklyExpenses + weeklyAnswer
+}
+
+for(let m = 0; m < monthlyExpenseQuestions.length; m++){
+    let monthlyAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[m]))
+    monthlyExpenses = monthlyExpenses + monthlyAnswer
+ }
